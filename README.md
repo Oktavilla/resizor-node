3883d18a823c77d38fdd24bf38fa6dfa48aedb1c
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2015-04-03 18:16:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+
+## Example usage
+
+```javascript
+var Resizor = require("resizor-node");
+var resizor = new Resizor("apikey");
+
+var addAsset = resizor.addAsset(fileStream);
+addAsset.then(function(assetData) {
+  var assetUrl = resizor.getAssetURL(assetData.id, {
+    size: "w200h200"
+  });
+  console.log(assetUrl);
+});
+```
+
+Would yield something like:
+```
+>> "https://resizor.com/assets/123456789.jpg?size=w200h200&token=w7cw847rtc7c7brc7rcb"
+```
+
+## Constructor
+
+The constructor takes two arguments: `fileStream` and `options`. Example and available options:
+```javascript
+var resizor = new Resizor("apikey", {
+  "useSSL": true,
+  "CDNHost": "myawesomecdn.resizor.com"
+});
+```
+
+`CDNHost` can be set if you're using your CloudFront with Resizor. See http://resizor.com/get-started
+
+## "addAsset" method
+
+addAssets takes a single argument: `fileStream`. It will return a promise. Upon success it will be resolved with data regarding the newly created asset.
+
+Example:
+```javascript
+var addAsset = resizor.addAsset(fileStream);
+addAsset.then(function(assetData) {
+  console.log(assetData);
+});
+```
+
+Would yield something like:
+```json
+{
+  "id": "78584756",
+  "name": "something.jpg",
+  "mimeType": "image/jpeg",
+  "size": 4534534,
+  "width": 500,
+  "height": 300
+}
+```
+
+## "deleteAsset" method
+
+deleteAsset takes a single argument: `id`. It will return a promise which will be resolved on success.
+
+Example:
+```javascript
+resizor.deleteAsset("78584756");
+```
+
+## "getAssetURL" method
+
+getAssetURL takes two arguments: `id` and `options`. It will return a valid, public, asset URL. The format of the URL is affected if you set `CDNHost` in the constructors `options`. 
+
+Example:
+```javascript
+resizor.getAssetURL("78584756", {
+  size: "w200h450" // See http://resizor.com/api#resize_syntax
+}
+```
---
+++ index.js
@@ -0,0 +1,121 @@
+"use strict";
+
+var assign = require("./lib/assign");
+var crypto = require("crypto");
+var Q = require("q");
+var request = require("request");
+
+var defaultOptions = {
+  apiHost: "resizor.com",
+  apiPort: "80",
+  useSSL: true,
+  CDNHost: null
+};
+
+// Resizor Class
+function Resizor(apiKey, options) {
+  if (!apiKey || (apiKey && typeof apiKey !== "string")) {
+    throw new Error("An Resizor.com apiKey is required.");
+  }
+
+  if (!(this instanceof Resizor)) {
+    return new Resizor(apiKey, options);
+  }
+
+  this.apiKey = apiKey;
+  this.options = defaultOptions;
+  if (options) {
+    this.set(options);
+  }
+
+
+  // PRIVATE METHODS
+  // ---------------
+
+  // Get the Resizor API URL
+  var getApiURL = (function() {
+    return (this.options.useSSL ? "https" : "http") + "://" + this.options.apiHost + ":" + (this.options.useSSL ? "443" : this.options.apiPort);
+  }).bind(this);
+
+  // Get a Resizor token for an asset
+  var getResizorToken = (function(id, assetOptions) {
+    return crypto.createHash("sha1").update(this.apiKey + "-" + id + "-" + assetOptions.size + "-" + assetOptions.format).digest("hex");
+  }).bind(this);
+
+  // Get a queryString style URL
+  var getQueryStringURL = function(id, assetOptions) {
+    var cutoutString = assetOptions.cutout ? "&cutout=" + assetOptions.cutout : "";
+    return getApiURL() + "/assets/" + id + "." + assetOptions.format + "?size=" + assetOptions.size + cutoutString + "&token=" + getResizorToken(id, assetOptions);
+  };
+
+  // Get a CDN compatible style URL
+  var getCdnCompatibleURL = (function(id, assetOptions) {
+    var cutoutString = assetOptions.cutout ? "/" + assetOptions.cutout : "";
+    return (this.options.useSSL ? "https" : "http") + "://" + this.options.CDNHost + "/assets/" + assetOptions.size + cutoutString + "/" + getResizorToken(id, assetOptions) + "/" + id + "." + assetOptions.format;
+  }).bind(this);
+
+
+  // PUBLIC METHODS
+  // --------------
+
+  // Get an asset URL
+  this.getAssetURL = function(id, assetOptions) {
+    assetOptions = assign({ size: "200", format: "jpg" }, assetOptions);
+    return this.options.CDNHost ? getCdnCompatibleURL(id, assetOptions) : getQueryStringURL(id, assetOptions);
+  };
+
+  // Add an asset
+  this.addAsset = Q.Promise(function(fileStream) {
+    if (fileStream) {
+      request.post({
+        url: getApiURL() + "/assets.json",
+        formData: {
+          "file": fileStream,
+          "api_key": this.apiKey
+        }
+      }, function(err, httpResponse, data) {
+        if (err) {
+          reject(new Error("Could not add asset"));
+        } else {
+          resolve({
+            id: data.asset.id,
+            name: data.asset.name + data.asset.extension,
+            mimeType: data.asset["mime_type"],
+            size: data.asset["file_size"],
+            width: data.asset.width,
+            height: data.asset.height
+          });
+        }
+      })
+    } else {
+      reject(new Error("Missing file stream"));
+    }
+  });
+
+  // Delete an asset
+  this.deleteAsset = Q.Promise(function(id) {
+    if (id && typeof id === "string") {
+      request.del({
+        url: getApiURL() + "/assets/" + id + ".json",
+        qs: { 
+          "api_key": this.apiKey
+        }
+      }, function(err, httpResponse) {
+        if (err) {
+          reject(new Error("Could not delete asset with id: " + id));
+        } else {
+          resolve();
+        }
+      });
+    } else {
+      reject(new Error("Missing or malformed id"));
+    }
+  });
+
+  // Set options
+  this.set = function(options) {
+    assign(this.options, options);
+  };
+}
+
+module.exports = Resizor;
